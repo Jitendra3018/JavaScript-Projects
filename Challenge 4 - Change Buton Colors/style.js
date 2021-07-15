dae7a725b6b6ad010616e480b84e19ee0cb918e5
Jitendra Garg <58606315+Jitendra3018@users.noreply.github.com>
2021-07-15 19:30:59 +0530

    Completed on June 18, 2021

---
+++ Challenge 4 - Change Buton Colors/style.js
@@ -0,0 +1,51 @@
+let all_buttons = document.getElementsByTagName('button');
+console.log(all_buttons);
+
+let copyAllButtons = [];
+for(let i = 0; i < all_buttons.length; i++) {
+    copyAllButtons.push(all_buttons[i].classList[1]);
+}
+console.log(copyAllButtons);
+
+function buttonColorChange(buttonThingy) {
+    console.log(buttonThingy.value);
+    if(buttonThingy.value === 'red') {
+        buttonRed();
+    } else if(buttonThingy.value === 'green') {
+        buttonGreen();
+    } else if(buttonThingy.value === 'reset') {
+        buttonReset();
+    } else if(buttonThingy.value === 'random') {
+        buttonRandom();
+    }
+}
+
+function buttonRed() {
+    for (let i = 0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add("btn-danger");
+    }
+}
+
+function buttonGreen() {
+    for (let i = 0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add("btn-success");
+    }
+}
+
+function buttonReset() {
+    for (let i = 0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyAllButtons[i]);
+    }
+}
+
+function buttonRandom() {
+    let choices = ['btn-primary', 'btn-danger', 'btn-warning', 'btn-success', 'btn-info'];
+    for(let i = 0; i < all_buttons.length; i++) {
+        let randomNumber = Math.floor(Math.random() * 5);
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(choices[randomNumber]);
+    }
+}
