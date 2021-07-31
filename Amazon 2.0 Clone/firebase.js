23b2b59eefcebee0e1121d6d6b4e3de18851a6a4
Jitendra Garg <58606315+Jitendra3018@users.noreply.github.com>
2021-08-01 03:11:19 +0530

    Completed on July 31, 2021

---
+++ Amazon 2.0 Clone/firebase.js
@@ -0,0 +1,15 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyDiV5wZmLQH51yoifdB7iaXEoyvL5KUNF8",
+    authDomain: "clone-2-94436.firebaseapp.com",
+    projectId: "clone-2-94436",
+    storageBucket: "clone-2-94436.appspot.com",
+    messagingSenderId: "955383708365",
+    appId: "1:955383708365:web:2e7b25c8fa521ecdb20784",
+    measurementId: "G-H255EPQVPD",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var db = firebase.firestore();
---
+++ Amazon 2.0 Clone/script.js
@@ -0,0 +1,117 @@
+function getItems() {
+    db.collection("items")
+        .get()
+        .then((querySnapshot) => {
+            // console.log(querySnapshot);
+            let items = [];
+            querySnapshot.forEach((doc) => {
+                // console.log(`${doc.id} => ${doc.data()}`);
+                items.push({
+                    id: doc.id,
+                    image: doc.data().image,
+                    name: doc.data().name,
+                    make: doc.data().make,
+                    rating: doc.data().rating,
+                    price: doc.data().price,
+                });
+                // console.table(items);
+            });
+            generateItems(items);
+        });
+}
+
+function generateItems(items) {
+    let itemsHTML = "";
+    items.forEach((item) => {
+        let doc = document.createElement("div");
+        doc.classList.add("main-product", "mr-5");
+        doc.innerHTML = `
+        <div
+        class="
+            product-image
+            w-48
+            h-52
+            bg-white
+            rounded-lg
+            p-4
+        "
+    >
+        <img
+            class="w-full h-full object-contain"
+            src="${item.image}"
+            alt=""
+        />
+    </div>
+    <div
+        class="
+            product-name
+            text-gray-700
+            font-bold
+            mt-2
+            text-sm
+        "
+    >
+        ${item.name}
+    </div>
+    <div class="product-make text-green-700">
+        ${item.make}
+    </div>
+    <div class="product-rating font-bold my-1">
+        🌟🌟🌟🌟🌟 ${item.rating}
+    </div>
+    <div
+        class="
+            product-price
+            font-bold
+            text-gray-700 text-lg
+        "
+    >${numeral(item.price).format("$0,0.00")}
+    </div>
+        `;
+        let addToCartEl = document.createElement("div");
+        addToCartEl.classList.add(
+            "hover:bg-yellow-600",
+            "cursor-pointer",
+            "product-add",
+            "h-8",
+            "w-28",
+            "rounded",
+            "bg-yellow-500",
+            "text-white",
+            "text-md",
+            "flex",
+            "justify-center",
+            "items-center"
+        );
+        addToCartEl.innerText = "Add to Cart";
+        addToCartEl.addEventListener("click", () => {
+            addToCart(item);
+        });
+        doc.appendChild(addToCartEl);
+        document.querySelector(".main-section-product").appendChild(doc);
+    });
+}
+
+function addToCart(item) {
+    // console.log("Add to Cart clicked");
+    // console.log(item);
+    let cartItem = db.collection("cart-items").doc(item.id);
+    cartItem.get().then(function (doc) {
+        if (doc.exists) {
+            cartItem.update({
+                quantity: doc.data().quantity + 1,
+            });
+        } else {
+            cartItem.set({
+                image: item.image,
+                make: item.make,
+                name: item.name,
+                rating: item.rating,
+                price: item.price,
+                quantity: 1,
+            });
+        }
+    });
+}
+
+getItems();
